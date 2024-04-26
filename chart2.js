caa796f5d02e7799a87576228f75d30f05ce7755
gucharrier <gu.charrier@gmail.com>
2024-04-26 09:35:49 +0200

    update-doughnut

---
+++ chart2.js
@@ -0,0 +1,12 @@
+const chart2 = document.getElementById('chart2').getContext('2d');
+const myChart2 = new Chart(chart2, {
+    type: 'doughnut',
+    data: {
+        labels : ['tv','bureau','tablette','appareil photo','Pc portable','mobile','écran'],
+        datasets: [{
+            label: 'products',
+            data: [60,40,20,50,80,90,30],
+            backgroundColor: ['#b4eaf6','#efcfff','#ffe69c','#79dfc1',]
+        }]
+    },
+})
