655f947994b048a3f5109674897966043f7484e8
gucharrier <gu.charrier@gmail.com>
2024-04-27 19:42:44 +0200

    update

--- chart2.js
+++ chart2.js
@@ -6,7 +6,7 @@ const myChart2 = new Chart(chart2, {
         datasets: [{
             label: 'products',
             data: [60,40,20,50,80,90,30],
-            backgroundColor: ['#b4eaf6','#efcfff','#ffe69c','#79dfc1',]
+            backgroundColor: ['#b4eaf6','#efcfff','#ffe69c','#79dfc1','#ffd4cf','#e284ff','#a1c6f7','#g5hr6k','#111111','#aaaaaa']
         }]
     },
 })
